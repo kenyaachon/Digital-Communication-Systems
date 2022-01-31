91ff21ae524ab8109ceb130b6ab67be4d3f60371
mosesmbugua <swahili1963@gmail.com>
2022-01-31 06:04:52 -0800

    Compression and Decompression Algo

--- ps1/PS1_lzw.py
+++ ps1/PS1_lzw.py
@@ -4,6 +4,15 @@ from optparse import OptionParser
 import struct
 import array
 
+
+def initializeArray():
+    table = []
+    for i in range(256):
+        # table[i] = chr(i)
+        # table[chr(i)] = i
+        table.append(chr(i))
+
+    return table
 def compress(filename):
     """
     Compresses a file using the LZW algorithm and saves output in another file.
@@ -12,7 +21,43 @@ def compress(filename):
     Returns:
         None.
     """
-    pass
+    try:
+        outputName = filename + '.zl'
+        table = initializeArray()
+        with open(filename, 'rb') as file:
+            #read one byte at a time when compressing a file
+            with open(outputName, 'wb') as outFile:
+                # string = array.array("B", file.read())
+                # symbol = array.array("B", file.read())
+                # while symbol:
+                # while symbol != '10':
+                #     if string + symbol in table:
+                #         string = string + symbol
+                #         print(string)
+                #     else:
+                #         # outFile.write(table.index(string))
+                #         table.append(string + symbol)
+                #         string = symbol
+                #     symbol = array.array("B", file.read())
+                # outFile.write(table.index(string))
+
+                compressed = array.array("B", file.read())
+                string = chr(compressed[0])
+                for symbol in compressed[1:]:
+                    symbol = chr(symbol)
+                    print(string + symbol)
+                    if string + symbol in table:
+                        string = string + symbol
+                        print(string)
+                    else:
+                        outFile.write(table.index(string).to_bytes(2, 'little'))
+                        table.append(string + symbol)
+                        string = symbol
+                outFile.write(table.index(string).to_bytes(2, 'little'))
+
+
+    except FileNotFoundError:
+        print ("This File %s does not exist", filename)
 
 def uncompress(filename):
     """
@@ -22,6 +67,28 @@ def uncompress(filename):
     Returns:
         None.
     """
+    try:
+        outputName = filename + ".u"
+        table = initializeArray()
+
+        with open(filename, 'rb') as file:
+            #read one byte at a time when compressing a file
+            with open(outputName, 'w') as outFile:
+                compressed = array.array("H", file.read())
+                string = table[compressed[0]]
+                outFile.write(string)
+                # print(compressed)
+                entry = ""
+                for code in compressed[1:]:
+                    if code >= len(table):
+                        entry = string + string[0]
+                    else:
+                        entry = table[code]
+                    outFile.write(entry)
+                    table.append(string+entry[0])
+                    string = entry
+    except FileNotFoundError:
+        print("This file %s does not exist", filename)
 
 if __name__ == '__main__':
     parser = OptionParser()
