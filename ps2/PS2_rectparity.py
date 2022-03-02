801678082970436cdb71c665751f9609c5d8b9db
mosesmbugua <swahili1963@gmail.com>
2022-03-01 21:36:50 -0800

    Final Recangular Parity Code

--- ps2/PS2_rectparity.py
+++ ps2/PS2_rectparity.py
@@ -5,11 +5,22 @@ import numpy
 
 def parity_rowCalc(codeword, nrows, ncols):
     row_errors = []
-    for i in xrange(1, nrows+1):
+    rowpos = 0
+    for i in range(1, nrows+1):
         total = 0
-        for j in xrange(1, ncols+1):
-            total += codeword[i*j - 1]
+        for j in range(1, ncols+1):
+            # print("row", i)
+            # print("column", j)
+
+            # print("data bit", codeword[rowpos + j - 1])
+            # print("data bit pos", rowpos + j - 1)
+            total += codeword[rowpos + j - 1 ]
+
+        rowpos += ncols
+        # print("row bit", codeword[nrows*ncols + i - 1])
+        # print("row bit pos", nrows*ncols + i - 1)
         total += codeword[nrows*ncols + i - 1]
+        # print(total)
         if total % 2 != 0:
             row_errors.append(i)
 
@@ -39,27 +50,44 @@ def rect_parity(codeword,nrows,ncols):
     columns = parity_columnCalc(codeword, nrows, ncols)
     rows = parity_rowCalc(codeword, nrows, ncols)
 
-    print "result from columns", columns
-    print "result from rows", rows
+    # print "result from columns", columns
+    # print "result from rows", rows
 
     if len(columns) == 0 and len(rows) == 0:
         return codeword[:ncols*nrows]
     elif len(columns) == 1 and len(rows) == 1:
+        print "columns", columns
+        print "rows", rows
+        print "nrows", nrows
+        print "ncols", ncols
         print "flipping the data bit"
         copy = codeword 
-        copy[columns[0]*rows[0] - 1] ^= 1
+        print(codeword)
+        flipbit = ((rows[0] - 1) * ncols) + columns[0] - 1
+        copy[flipbit] ^= 1
         return copy[:ncols*nrows]
     elif (len(columns) == 1 and len(rows) == 0) or (len(columns) == 0 and len(rows) == 1):
         return codeword[:ncols*nrows]
     else:
+        # print "size of the data", ncols*nrows + ncols + nrows
+        # print "number of rows", nrows
+        # print "number of cols", ncols
+        # print "columns", columns
+        # print "row", rows
         return codeword[:ncols*nrows]
 if __name__ == '__main__':
-    # PS2_tests.test_correct_errors(rect_parity)
+    PS2_tests.test_correct_errors(rect_parity)
     # codeword = [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0]
     # rect_parity(codeword, 2, 4)
 
     # codeword = [1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0]
-    # rect_parity(codeword, 2, 4)
+    # print(rect_parity(codeword, 2, 4))
 
-    codeword = [0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1]
-    print(rect_parity(codeword, 2, 4))
+    # codeword = [0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1]
+    # print(rect_parity(codeword, 2, 4))
+
+    # codeword = [ 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # print(rect_parity(codeword, 4, 2))
+
+    # codeword = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # print(rect_parity(codeword, 4, 2))
